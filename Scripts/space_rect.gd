1fffe482bd7a06780aa56efd3364cd63e8db8ce2
CookieCherith0969 <cookiecherith@gmail.com>
2024-12-05 06:06:30 +1100

    Space in Maps

---
+++ Scripts/space_rect.gd
@@ -0,0 +1,16 @@
+extends TextureRect
+
+@export
+var do_flipping : bool = true
+@export
+var do_rotating : bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if do_flipping:
+		flip_h = randi() % 2 == 0
+		flip_v = randi() & 2 == 0
+	
+	if do_rotating:
+		pivot_offset = size/2
+		rotation = randi_range(0,3)*PI/2
